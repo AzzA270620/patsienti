18a0506694e9c8096327bcc02d6e7cb51f16401f
AzzA270620 <g1wp1zgj5fv1@mail.ru>
2021-11-03 09:38:51 +0300

    новые

---
+++ patsient/PatientCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace patsient
+{
+    public class PatientCart
+    {
+        private int Patientid;
+        private string PatientFull_Name;
+        private string PatienSex;
+        private string PatientAge;
+        private diagnos diagnoz;
+        private string PatientEntrancee;
+        private int PatientHeight;
+        private string PatientSpesialS;
+        private string others;
+    }
+    }
+
